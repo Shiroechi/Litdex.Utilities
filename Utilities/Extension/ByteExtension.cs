5db477cd929342c83ec5999824fd3e10275d54eb
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2020-12-18 10:22:36 +0700

    Create ByteExtension.cs

---
+++ Utilities/Extension/ByteExtension.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using Litdex.Utilities.Base;
+
+namespace Litdex.Utilities.Extension
+{
+	/// <summary>
+	/// Byte[] Extension.
+	/// </summary>
+	public static class ByteExtension
+	{
+		public static string ToBase16(this byte[] bytes, bool upper = true)
+		{
+			return Base16.Encode(bytes, upper);
+		}
+
+		public static string ToBase64(this byte[] bytes)
+		{
+			return Base64.Encode(bytes);
+		}
+
+		public static string ToBase85(this byte[] bytes)
+		{
+			return Base85.Encode(bytes);
+		}
+
+		public static string ToBase91(this byte[] bytes)
+		{
+			return Base91.Encode(bytes);
+		}
+
+		/// <summary>
+		/// Clear array.
+		/// </summary>
+		/// <param name="arr"></param>
+		public static void Clear(this Array arr)
+		{
+			Array.Clear(arr, 0, arr.Length);
+		}
+
+		public static byte[] SubByte(this byte[] arr, int offset)
+		{
+			return arr.SubByte(offset, arr.Length);
+		}
+
+		public static byte[] SubByte(this byte[] arr, int ffset, int length)
+		{
+			var temp = new byte[length - ffset];
+			Array.Copy(arr, ffset, temp, 0, length);
+			return temp;
+		}
+	}
+}
