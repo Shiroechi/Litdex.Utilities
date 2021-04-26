bfdbce81caabce6c4e8850f09faa0ce797baa9d2
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2021-04-26 15:55:34 +0700

    refactor code

---
+++ Source/Utilities/Extension/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Litdex.Utilities.Extension
+{
+	/// <summary>
+	///		String Extension
+	/// </summary>
+	public static class StringExtension
+	{
+		/// <summary>
+		///		Convert <see cref="string"/> to an array of <see cref="byte"/>.
+		/// </summary>
+		/// <param name="str">
+		///		A <see cref="string"/> to convert.
+		/// </param>
+		/// <returns>
+		///		An array of <see cref="byte"/>s from <see cref="string"/>.
+		///	</returns>
+		public static byte[] GetBytes(this string str)
+		{
+			return System.Text.Encoding.UTF8.GetBytes(str);
+		}
+	}
+}
