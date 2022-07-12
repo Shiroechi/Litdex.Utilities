ea9e55cafd572c8ea4507bacaae03715ed1ef1ca
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2022-07-12 14:04:29 +0700

    Tweak Base16 algorithm
    - Remove tab in documentation
    - Add compatibility for .Net Standard 2.1 and .Net 5 upward

--- Source/Utilities/BinaryEncoding/Base16.cs
+++ Source/Utilities/BinaryEncoding/Base16.cs
@@ -3,24 +3,58 @@
 namespace Litdex.Utilities.BinaryEncoding
 {
 	/// <summary>
-	///		Encode and decode in base 16 (hexadecimal).
+	///	Encode and decode in base 16 (hexadecimal).
 	/// </summary>
 	public static class Base16
 	{
+#if NET5_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+		private static ReadOnlySpan<byte> UpperCaseTable => new[]
+		{
+			(byte)'0', (byte)'1', (byte)'2', (byte)'3',
+			(byte)'4', (byte)'5', (byte)'6', (byte)'7',
+			(byte)'8', (byte)'9', (byte)'A', (byte)'B',
+			(byte)'C', (byte)'D', (byte)'E', (byte)'F',
+		};
+
+		private static ReadOnlySpan<byte> LowerCaseTable => new[]
+		{
+			(byte)'0', (byte)'1', (byte)'2', (byte)'3',
+			(byte)'4', (byte)'5', (byte)'6', (byte)'7',
+			(byte)'8', (byte)'9', (byte)'a', (byte)'b',
+			(byte)'c', (byte)'d', (byte)'e', (byte)'f',
+		};
+#else
+		private static readonly char[] UpperCaseTable =
+		{
+			'0', '1', '2', '3',
+			'4', '5', '6', '7',
+			'8', '9', 'A', 'B',
+			'C', 'D', 'E', 'F'
+		};
+
+		private static readonly char[] LowerCaseTable =
+		{
+			'0', '1', '2', '3',
+			'4', '5', '6', '7',
+			'8', '9', 'a', 'b',
+			'c', 'd', 'e', 'f'
+		};
+#endif
+
 		/// <summary>
-		///		Encode <paramref name="bytes"/> to hexadecimal <see cref="string"/>.
+		///	Encode <paramref name="bytes"/> to hexadecimal <see cref="string"/>.
 		/// </summary>
 		/// <param name="bytes">
-		///		Array of <see cref="byte"/>s to encode.
+		///	Array of <see cref="byte"/>s to encode.
 		///	</param>
 		/// <param name="upperCase">
-		///		Hexadecimal output <see cref="string"/> letter case. Default is <see langword="true"/> to upper case.
+		///	Hexadecimal output <see cref="string"/> letter case. Default is <see langword="true"/> to upper case.
 		///	</param>
 		/// <returns>
-		///		Hexadecimal <see cref="string"/>.
+		///	Hexadecimal <see cref="string"/>.
 		/// </returns>
 		/// <exception cref="ArgumentNullException">
-		///		<paramref name="bytes"/> is null or empty.
+		///	<paramref name="bytes"/> is null or empty.
 		/// </exception>
 		public static string Encode(byte[] bytes, bool upperCase = true)
 		{
@@ -37,34 +71,29 @@ namespace Litdex.Utilities.BinaryEncoding
 				return EncodeUpper(bytes);
 #endif
 			}
-#if NET5_0_OR_GREATER
-			return Convert.ToHexString(bytes).ToLower();
-#else
 			return EncodeLower(bytes);
-#endif
 		}
 
 		/// <summary>
-		///		Encode array of <see cref="byte"/>s to hexadecimal <see cref="string"/> in upper case.
+		///	Encode array of <see cref="byte"/>s to hexadecimal <see cref="string"/> in upper case.
 		/// </summary>
 		/// <param name="bytes">
-		///		Array of <see cref="byte"/>s to encode.
+		///	Array of <see cref="byte"/>s to encode.
 		///	</param>
 		/// <returns>
-		///		Hexadecimal <see cref="string"/> in upper case.
+		///	Hexadecimal <see cref="string"/> in upper case.
 		/// </returns>
 		private static string EncodeUpper(byte[] bytes)
 		{
-			var c = new char[bytes.Length * 2];
-			int b;
-			for (var i = 0; i < bytes.Length; i++)
+			var result = new char[bytes.Length * 2];
+			for (var i = 0; i < bytes.Length; ++i)
 			{
-				b = bytes[i] >> 4;
-				c[i * 2] = (char)(55 + b + (((b - 10) >> 31) & -7));
-				b = bytes[i] & 0xF;
-				c[(i * 2) + 1] = (char)(55 + b + (((b - 10) >> 31) & -7));
+				var j = i * 2;
+				result[j] = (char)UpperCaseTable[bytes[i] >> 4];
+				result[j + 1] = (char)UpperCaseTable[bytes[i] & 0xF];
 			}
-			return new string(c);
+
+			return new string(result);
 		}
 
 		/// <summary>
@@ -78,32 +107,43 @@ namespace Litdex.Utilities.BinaryEncoding
 		/// </returns>
 		private static string EncodeLower(byte[] bytes)
 		{
-			var c = new char[bytes.Length * 2];
-			int b;
-			for (var i = 0; i < bytes.Length; i++)
+#if NET5_0_OR_GREATER || NETSTANDARD2_1_OR_GREATER
+			var result = bytes.Length * 2 < 32 ? stackalloc char[bytes.Length * 2] : new char[bytes.Length * 2];
+			for (var i = 0; i < bytes.Length; ++i)
 			{
-				b = bytes[i] >> 4;
-				c[i * 2] = (char)(87 + b + (((b - 10) >> 31) & -39));
-				b = bytes[i] & 0xF;
-				c[(i * 2) + 1] = (char)(87 + b + (((b - 10) >> 31) & -39));
+				var j = i * 2;
+				result[j] = (char)LowerCaseTable[bytes[i] >> 4];
+				result[j + 1] = (char)LowerCaseTable[bytes[i] & 0xF];
 			}
-			return new string(c);
+
+			return new string(result);
+#else
+			var result = new char[bytes.Length * 2];
+			for (var i = 0; i < bytes.Length; ++i)
+			{
+				var j = i * 2;
+				result[j] = LowerCaseTable[bytes[i] >> 4];
+				result[j + 1] = LowerCaseTable[bytes[i] & 0xF];
+			}
+
+			return new string(result);
+#endif
 		}
 
 		/// <summary>
-		///		Decode hexadecimal <see cref="string"/> to array of <see cref="byte"/>s.
+		///	Decode hexadecimal <see cref="string"/> to array of <see cref="byte"/>s.
 		/// </summary>
 		/// <param name="hexString">
-		///		Hexadecimal <see cref="string"/> to decode.
+		///	Hexadecimal <see cref="string"/> to decode.
 		///	</param>
 		/// <returns>
-		///		Array of <see cref="byte"/>s from decoded <paramref name="hexString"/>.
+		///	Array of <see cref="byte"/>s from decoded <paramref name="hexString"/>.
 		/// </returns>
 		/// <exception cref="ArgumentNullException">
-		///		<paramref name="hexString"/> is null, empty or only containing white spaces.
+		///	<paramref name="hexString"/> is null, empty or only containing white spaces.
 		/// </exception>
 		/// <exception cref="ArgumentOutOfRangeException">
-		///		<paramref name="hexString"/> length is odd.
+		///	<paramref name="hexString"/> length is odd.
 		/// </exception>
 		public static byte[] Decode(string hexString)
 		{
@@ -125,27 +165,27 @@ namespace Litdex.Utilities.BinaryEncoding
 			{
 				high = hexString[i * 2];
 				low = hexString[i * 2 + 1];
-				high = (high & 0xf) + ((high & 0x40) >> 6) * 9;
-				low = (low & 0xf) + ((low & 0x40) >> 6) * 9;
+				high = (high & 0xF) + ((high & 0x40) >> 6) * 9;
+				low = (low & 0xF) + ((low & 0x40) >> 6) * 9;
 				result[i] = (byte)((high << 4) | low);
 			}
 			return result;
 		}
 
 		/// <summary>
-		///		Decode hexadecimal <see cref="string"/> to array of <see cref="byte"/>s.
+		///	Decode hexadecimal <see cref="string"/> to array of <see cref="byte"/>s.
 		/// </summary>
 		/// <param name="hexString">
-		///		Hexadecimal <see cref="string"/> to decode.
+		///	Hexadecimal <see cref="string"/> to decode.
 		///	</param>
 		/// <returns>
-		///		Array of <see cref="byte"/>s from decoded <paramref name="hexString"/>.
+		///	Array of <see cref="byte"/>s from decoded <paramref name="hexString"/>.
 		/// </returns>
 		/// <exception cref="ArgumentNullException">
-		///		<paramref name="hexString"/> is null, empty or only containing white spaces.
+		///	<paramref name="hexString"/> is null, empty or only containing white spaces.
 		/// </exception>
 		/// <exception cref="ArgumentOutOfRangeException">
-		///		<paramref name="hexString"/> length is odd.
+		///	<paramref name="hexString"/> length is odd.
 		/// </exception>
 		public static byte[] Decode(char[] hexString)
 		{
@@ -167,8 +207,8 @@ namespace Litdex.Utilities.BinaryEncoding
 			{
 				high = hexString[i * 2];
 				low = hexString[i * 2 + 1];
-				high = (high & 0xf) + ((high & 0x40) >> 6) * 9;
-				low = (low & 0xf) + ((low & 0x40) >> 6) * 9;
+				high = (high & 0xF) + ((high & 0x40) >> 6) * 9;
+				low = (low & 0xF) + ((low & 0x40) >> 6) * 9;
 				result[i] = (byte)((high << 4) | low);
 			}
 			return result;
